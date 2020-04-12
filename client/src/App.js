019ef4b8ce3123a94bdd2830ef85289dc0984b74
DN <dilaran@live.com>
2020-04-12 16:24:15 -0700

    minor

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import React, { Component } from "react";
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Search}/>
+            <Route path="/saved" component={Saved}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,25 +1,16 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  getTitles: function(bookTitle) {
-    return axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=title:${bookTitle}`
-    );
-  }
-};
+    getBooks: () => {
+        return axios.get("/api/books");
+    },
+    searchBooks: (title) => {
+        return axios.post("/search", {title: title});
+    },
+    addBookToDB: (bookData) => {
+        return axios.post("/api/books", bookData);
+    },
+    deleteBook: (id) => {
+        return axios.delete(`/api/books/${id}`);
+    }
+}
